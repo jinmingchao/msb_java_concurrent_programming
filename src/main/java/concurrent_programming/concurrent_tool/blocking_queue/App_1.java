93b716ed0f3b00f814fa1c5270d81ca862cc1c05
jinmingchao <441821724@qq.com>
2024-05-27 17:26:24 +0800

    20240523-1417

---
+++ src/main/java/concurrent_programming/concurrent_tool/blocking_queue/App_1.java
@@ -0,0 +1,10 @@
+package concurrent_programming.concurrent_tool.blocking_queue;
+
+import java.util.concurrent.DelayQueue;
+
+public class App_1 {
+    public static void main(String[] args) {
+        DelayQueue q = new DelayQueue();
+        q.poll();
+    }
+}
