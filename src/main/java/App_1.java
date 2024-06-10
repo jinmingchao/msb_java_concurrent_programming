97885a9cfb8ee05d7f7802e6d54180ed09a676e0
jinmingchao <441821724@qq.com>
2024-06-11 06:29:57 +0800

    112222

---
+++ src/main/java/App_1.java
@@ -0,0 +1,12 @@
+public class App_1 {
+    public static void main(String[] args) {
+
+//        for (int i = 0; i <= 10000; i++) {
+//
+//        }
+        int a = -1021;
+        System.out.println(a >>> 1);
+        System.out.println(a >> 1);
+        System.out.println(a << 1);
+    }
+}
