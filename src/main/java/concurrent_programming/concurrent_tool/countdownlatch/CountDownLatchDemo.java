c2b62f0a68af84d1794bcf0aa228d1a359da2478
jinmingchao <441821724@qq.com>
2024-06-11 17:27:29 +0800

    202406111727

--- src/main/java/concurrent_programming/concurrent_tool/countdownlatch/CountDownLatchDemo.java
+++ src/main/java/concurrent_programming/concurrent_tool/countdownlatch/CountDownLatchDemo.java
@@ -1,4 +1,4 @@
-package concurrent_programming.concurrent_tool;
+package concurrent_programming.concurrent_tool.countdownlatch;
 
 public class CountDownLatchDemo {
     public static void main(String[] args) {
--- src/main/java/concurrent_programming/concurrent_tool/cyclicbarrier/CycleBarrierDemo.java
+++ src/main/java/concurrent_programming/concurrent_tool/cyclicbarrier/CycleBarrierDemo.java
@@ -1,4 +1,4 @@
-package concurrent_programming.concurrent_tool;
+package concurrent_programming.concurrent_tool.cyclicbarrier;
 
 import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
